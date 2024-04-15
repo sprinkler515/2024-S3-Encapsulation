a470b74fd261918f46acbbfa629b22d5cefebf0c
Tommy Chanthavisouk <wildcodeschool.sprinkler515@pm.me>
2024-04-15 14:01:19 +0200

    create main program

---
+++ BeerEncapsulator.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _0413_S3_Encapsulation
+{
+    internal class BeerEncapsulator
+    {
+        private int _availableBottles = 0;
+        private int _availableCapsules = 0;
+        private double _availableBeerVolume = 0;
+
+        public int SetAvailableBottles(int bottles) =>
+            _availableBottles = bottles;
+
+        public int SetAvailableCapsules(int capsules) =>
+            _availableCapsules = capsules;
+
+        public double AddBeer(double volume) =>
+            _availableBeerVolume += Math.Abs(volume);
+
+        public int MaxBeerBottles() =>
+            (int)_availableBeerVolume / 33;
+
+        public int ProduceEncapsulatedBeerBottles(int quantity)
+        {
+            int bottles = _availableBottles;
+            int capsules = _availableCapsules;
+            int encapsulatedBottles = Math.Min(bottles, capsules);
+
+            if (encapsulatedBottles < quantity)
+            {
+                Console.WriteLine("Insufficent quantity of encapsulated empty bottles.");
+                RequiredComponents("bottle", bottles, quantity);
+                RequiredComponents("capsule", capsules, quantity);
+                return 0;
+            }
+            if (MaxBeerBottles() < quantity)
+            {
+                Console.WriteLine($"Insufficient volume of beer ({quantity * 33 - _availableBeerVolume:N2}cl).");
+                return 0;
+            }
+            return quantity;
+        }
+
+        public void DisplayComponents()
+        {
+            Console.WriteLine($"Bottles available: {_availableBottles}");
+            Console.WriteLine($"Capsules available: {_availableCapsules}");
+            Console.WriteLine($"Volume available: {_availableBeerVolume:N2}\n");
+        }
+
+        public double GetBeerVolume() => _availableBeerVolume;
+
+        public static void RequiredComponents(string component, int quantityComponent, int quantityRequired)
+        {
+            int missingQuantity = quantityRequired - quantityComponent;
+
+            if (missingQuantity > 0)
+                Console.WriteLine($"Insufficient quantity of {missingQuantity} {component} components.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Globalization;
+
+namespace _0413_S3_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity, beerProduced;
+            CultureInfo culture = new("en-US");
+            BeerEncapsulator beerEncapsulator = new();
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            SetComponents(beerEncapsulator);
+            PromptAddBeer(beerEncapsulator);
+            beerEncapsulator.DisplayComponents();
+
+            Console.Write("How many bottles of beers do you want to make: ");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            beerProduced = beerEncapsulator.ProduceEncapsulatedBeerBottles(quantity);
+
+            if (beerProduced == 0)
+                Console.WriteLine("No beer bottles were produced.");
+            else
+                Console.WriteLine($"Success! Bottle of beer produced: {beerProduced}");
+        }
+
+        static int ValidPositiveInt(int entry)
+        {
+            while (entry <= 0)
+            {
+                Console.Write("Invalid value. Please enter a positive number: ");
+                entry = Convert.ToInt32(Console.ReadLine());
+            }
+            return entry;
+        }
+
+        static double ValidPositiveDouble(double entry)
+        {
+            while (entry < 33)
+            {
+                Console.Write("Invalid value. Please enter a positive number: ");
+                entry = Convert.ToDouble(Console.ReadLine());
+            }
+            return entry;
+        }
+
+        static void PromptAddBeer(BeerEncapsulator beerEncapsulator)
+        {
+            string answer;
+            bool answered = false;
+
+            while (!answered)
+            {
+                Console.Write("Do you want to add additional beer? (y/n) ");
+                answer = Console.ReadLine() ?? "";
+                if (answer == "y")
+                {
+                    Console.Write("\nAdd additional beer: ");
+                    beerEncapsulator.AddBeer(Convert.ToInt32(Console.ReadLine()));
+                    Console.WriteLine($"New volume available: {beerEncapsulator.GetBeerVolume():N2}\n");
+                }
+                else if (answer == "n") answered = true;
+                else Console.WriteLine("Unknown command. Try again.\n");
+            }
+            Console.WriteLine();
+        }
+
+        static void SetComponents(BeerEncapsulator beerEncapsulator)
+        {
+            int bottles, capsules;
+            double beerVolume;
+
+            Console.Write("Bottles available (min: 1): ");
+            bottles = Convert.ToInt32(Console.ReadLine());
+            bottles = ValidPositiveInt(bottles);
+
+            Console.Write("Capsules available (min: 1): ");
+            capsules = Convert.ToInt32(Console.ReadLine());
+            capsules = ValidPositiveInt(capsules);
+
+            Console.Write("Volume of beer available (min: 33): ");
+            beerVolume = Convert.ToDouble(Console.ReadLine());
+            beerVolume = ValidPositiveDouble(beerVolume);
+            Console.WriteLine();
+            beerEncapsulator.SetAvailableBottles(bottles);
+            beerEncapsulator.SetAvailableCapsules(capsules);
+            beerEncapsulator.AddBeer(beerVolume);
+        }
+    }
+}
